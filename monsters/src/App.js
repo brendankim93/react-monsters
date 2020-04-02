bfaabc760389b23fcd23a23f46be9e7c1757bce4
Brendan Kim <brendankim93@yahoo.com>
2020-04-02 14:46:58 -0700

    changes

--- monsters/src/App.js
+++ monsters/src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import { CardList } from "./components/card-list/card-list.component";
+import { SearchBox } from "./components/search-box/search-box.component";
 import './App.css';
+
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     }
   }
 
@@ -17,9 +20,17 @@ class App extends Component {
     .then(users => this.setState({ monsters: users }))
   }
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
       return (
         <div className='App'>
-          <CardList monsters={this.state.monsters} />
+          <SearchBox 
+          placeholder='search monsters'
+          handleChange={e => this.setState({ searchField: e.target.value })}
+          />
+          <CardList monsters={filteredMonsters} />
         </div>
       );
   }
