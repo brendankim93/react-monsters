44a20d5d9829cbd0e1cc296f06baf563cea24cf1
Brendan Kim <brendankim93@yahoo.com>
2020-04-09 20:50:09 -0700

    Updates

---
+++ precache-manifest.f8ce34e8d06a16ff4a28f5a14c86d5d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70345dbdff43107feefa2a495d786f0d",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "f99c02c5a026e85d1dea",
+    "url": "/monsters/static/css/main.e397d6bf.chunk.css"
+  },
+  {
+    "revision": "2b7d9edd948003b96165",
+    "url": "/monsters/static/js/2.b6796cae.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters/static/js/2.b6796cae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f99c02c5a026e85d1dea",
+    "url": "/monsters/static/js/main.73b7245d.chunk.js"
+  },
+  {
+    "revision": "69ad86de3903771917ee",
+    "url": "/monsters/static/js/runtime-main.5e6993dc.js"
+  }
+]);
